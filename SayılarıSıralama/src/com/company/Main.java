763097d837746d35ad4b83ab876ae764ba2ce3dd
dilanergin <dilanrgn@gmail.com>
2022-02-21 13:06:34 +0300

    Sayıları sıralama

---
+++ SayılarıSıralama/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.
+        int a, b, c;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen üç adet sayı giriniz:");
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println(a + " > " + b + " > " + c);
+            } else {
+                System.out.println(a + " > " + c + " > " + b);
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println(b + " > " + a + " > " + c);
+            } else {
+                System.out.println(b + " > " + c + " > " + a);
+            }
+        } else if ((c > a) && (c > b)) {
+            if (a > b) {
+                System.out.println(c + " > " + a + " > " + b);
+            } else {
+                System.out.println(c + " > " + b + " > " + a);
+            }
+
+        }
+    }
+
+
+}
